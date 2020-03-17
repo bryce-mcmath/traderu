65de7b8c1e26a11b12778825efaf034ddf520a27
Jon Langlois <jonathan-langlois@live.ca>
2020-03-16 22:56:43 -0700

    fixes comma in seeds, adds update values query

--- server/db/04_Portfolios_Stocks.sql
+++ server/db/04_Portfolios_Stocks.sql
@@ -4,6 +4,6 @@ CREATE TABLE portfolios_stocks (
   id serial PRIMARY KEY NOT NULL,
   stock_id integer NOT NULL REFERENCES stocks (id) ON DELETE CASCADE,
   portfolio_id integer NOT NULL REFERENCES portfolios (id) ON DELETE CASCADE,
-  quantity integer NOT NULL
+  quantity integer NOT NULL,
   unique (portfolio_id, stock_id)
 );
---
+++ server/db/updates/update_portfolio_value.sql
@@ -0,0 +1,32 @@
+
+UPDATE
+  portfolios
+SET
+  value = (
+    -- close: close value of most recent stock date in intraday
+    -- quantitiy: quantity of stock in portfolios_stocks
+    -- if sum is null (not holding stocks), set to 0
+    SELECT
+      COALESCE (SUM(close::numeric * quantity), 0)
+    FROM (
+      -- Select stock id and close value from stock_histories
+      SELECT
+        stock_id,
+        time_series_intraday -> 'Time Series (5min)' -> (
+          --select the most recent date (key) in the intraday JSON
+          SELECT
+            intraday_key
+          FROM
+            -- JSON key, value pairs as rows in fields intraday_key, v
+            json_each(time_series_intraday -> 'Time Series (5min)') AS j (intraday_key,
+              v)
+          ORDER BY
+            --set key type as timestamp, order descending (backwards in time), take first 
+            intraday_key::timestamp DESC
+          LIMIT 1) ->> '4. close' AS close
+      FROM
+        stock_histories) AS close_value
+      JOIN portfolios_stocks ON close_value.stock_id = portfolios_stocks.stock_id
+    WHERE
+      --value includes held cash
+      portfolio_id = portfolios.id) + portfolios.cash
