f36159655f1cf0ec470d87f135c8880baf59ee18
wilwong89 <wilwong89@gmail.com>
2020-03-23 04:39:46 -0700

    Added symbol search feature

--- client/src/api/ajaxCalls.js
+++ client/src/api/ajaxCalls.js
@@ -1,36 +1,44 @@
 import Axios from 'axios';
 
 export default {
-	loginAuth(email = '', password = '') {
-		return Axios.post('/api/login', {
-			email: email,
-			password: password
-		})
-			.then(response => response.data)
-			.catch(err => err);
-	},
+  loginAuth(email = '', password = '') {
+    return Axios.post('/api/login', {
+      email: email,
+      password: password
+    })
+      .then(response => response.data)
+      .catch(err => err);
+  },
 
-	fetchStocksData() {
-		//Local route for quicker workflow during development
-		// return Axios.get('http://localhost:8002/api/stocks')
-		return Axios.get('/api/stocks').then(res => {
-			return res.data.map(stockObject => {
-				return {
-					name: stockObject.name,
-					prices: stockObject.stockdata.map(stock =>
-						Number(stock.data['4. close'])
-					)
-				};
-			});
-		});
-	},
+  fetchStocksData() {
+    //Local route for quicker workflow during development
+    // return Axios.get('http://localhost:8002/api/stocks')
+    return Axios.get('/api/stocks').then(res => {
+      return res.data.map(stockObject => {
+        return {
+          name: stockObject.name,
+          prices: stockObject.stockdata.map(stock =>
+            Number(stock.data['4. close'])
+          )
+        };
+      });
+    });
+  },
 
-	fetchRankingsData() {
-		//Local route for quicker workflow during development
-		// return Axios.get('http://localhost:8002/api/leaderboard')
-		return Axios.get('/api/leaderboard').then(res => {
-			console.log(res.data.rankings);
-			return res.data.rankings;
-		});
-	}
+  fetchStockSymbolData(symbol) {
+    //Local route for quicker workflow during development
+    // return Axios.get('http://localhost:8002/api/stocks')
+    return Axios.get(`/api/stocks/${symbol}`)
+      .then(res => res.data)
+      .catch(err => err);
+  },
+
+  fetchRankingsData() {
+    //Local route for quicker workflow during development
+    // return Axios.get('http://localhost:8002/api/leaderboard')
+    return Axios.get('/api/leaderboard').then(res => {
+      console.log(res.data.rankings);
+      return res.data.rankings;
+    });
+  }
 };
