85a1aab5493139a2ca0f131314b5650176b7d727
Jon Langlois <jonathan-langlois@live.ca>
2020-03-23 14:07:13 -0700

    Add button for new portfolio form

---
+++ client/src/components/new_portfolio_form/NewPortfolioForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-form
+    ref="form"
+    v-model="valid"
+    lazy-validation
+  >
+    <v-text-field
+      v-model="name"
+      :counter="10"
+      :rules="nameRules"
+      label="Name"
+      required
+    ></v-text-field>
+  </v-form>
+</template>
+
+<script>
+  import { mapActions } from 'vuex';
+
+  export default {
+    props: {
+    },
+    methods: {
+    },
+    computed: {
+    }
+  };
+</script>
--- client/src/views/Portfolios.vue
+++ client/src/views/Portfolios.vue
@@ -1,7 +1,9 @@
 <template>
   <main class="view-container--alt">
     <h2>Primary Header</h2>
-    <v-btn block :dark="dark">Add Portfolio</v-btn>
+    <v-btn block :dark="dark" @click="test">Add Portfolio</v-btn>
+    <Dialog>
+    </Dialog>
     <v-expansion-panels :accordion="true" :focusable="true" :flat="true" :dark="dark">
       <v-expansion-panel
         v-for="(portfolio,i) in portfolios"
@@ -19,10 +21,12 @@
 <script>
   import { mapActions } from 'vuex';
   import Portfolio from '../components/portfolio/Portfolio.vue'
+  // import Dialog from '../components/dialog/Dialog.vue'
+  import NewPortfolioForm from '../components/new_portfolio_form/NewPortfolioForm.vue'
 
   export default {
     name: 'Portfolios',
-    components: {Portfolio},
+    components: {Portfolio}, //, Dialog},
     created(){
       //@TODO: think of better logic for this
       //Don't want to reload if current,
@@ -36,7 +40,23 @@
       }
     },
     methods: {
-      ...mapActions(['setUserPortfolios'])
+      ...mapActions(['setUserPortfolios']),
+
+      // test() {
+			// 	this.$store.commit('setDialogText', {
+			// 		title: 'Test title',
+			// 		content: <NewPortfolioForm />,
+			// 		primaryBtn: 'Yo',
+			// 		primaryCallback: function() {
+			// 			console.log('Called back yo');
+			// 		},
+			// 		secondaryBtn: 'No',
+			// 		secondaryCallback: function() {
+			// 			console.log('No called back');
+			// 		}
+			// 	});
+			// 	this.$store.commit('setShowDialog', true);
+			// }
     },
     computed: {
       portfolios(){
