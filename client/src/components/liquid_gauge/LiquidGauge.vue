ca118b6f3c6e83fb2aadcca28caabbb5dbe12c33
Bryce McMath <bryce.j.mcmath@gmail.com>
2020-03-27 00:26:28 -0700

    Scaffold LiquidGauge.vue and stylesheet, with props

---
+++ client/src/components/liquid_gauge/LiquidGauge.vue
@@ -0,0 +1,38 @@
+<template>
+  <svg
+    :id="id"
+    width="960"
+    height="500"
+    viewBox="0 0 960 500"
+    preserveAspectRatio="xMidYMid meet"
+  ></svg>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+
+  export default Vue.extend({
+    name: 'LiquidGauge',
+    props: {
+      id: {
+        type: String,
+        default: 'gauge'
+      },
+      percentile: {
+        type: Number,
+        default: 74
+      }
+    },
+    computed: {
+      dark() {
+        return this.$store.state.ui.dark;
+      }
+    },
+    methods: {},
+    mounted() {}
+  });
+</script>
+
+<style lang="scss" scoped>
+  @import 'liquid_gauge';
+</style>
