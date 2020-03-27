67aac471030122a58ad926f605e9f26b32be4899
Jon Langlois <jonathan-langlois@live.ca>
2020-03-27 09:11:54 -0700

    Add landing page
    What:
    
    1) Add InfoSlider component
    2) Add InfoList component
    3) Add main content block

---
+++ client/src/components/info-list/InfoList.vue
@@ -0,0 +1,32 @@
+<template>
+  <main>
+    <h1>Build your dream portfolio.</h1>
+    <div class="card">
+      <h2><i class="fas fa-user-plus"></i>Register for free</h2>
+      <p class="subtitle">Create an account for free today and start trading instantly.</p>
+    </div>
+    <div class="card">
+      <h2><i class="fa fa-chart-line"></i>Create a portfolio</h2>
+      <p class="subtitle">Create one or more portfolios to test out different trading strategies. </p>
+    </div>
+    <div class="card">
+      <h2><i class="fas fa-balance-scale-left"></i>Buy your assets</h2>
+      <p class="subtitle"> Choose from hundreds of stocks and cryptocurrencies </p>
+    </div>
+    <div class="card">
+      <h2><i class="fas fa-trophy"></i>Compete with friends</h2>
+      <p class="subtitle"> Track your portfolios performance against others, and see which strategies end up on top. </p>
+    </div>
+  </main>
+</template>
+
+<script>
+
+export default {
+
+};
+</script>
+
+<style scoped lang="scss">
+  @import 'info_list'
+</style>
--- client/src/views/Info.vue
+++ client/src/views/Info.vue
@@ -1,20 +1,35 @@
 <template>
-  <main class="home view-container">
-    <v-btn :dark="dark" @click="showStocksDrawer">View Stocks</v-btn>
-    <StocksListDrawer />
+  <main>
+    <div id="main-content">
+      <div id="intro-text">
+        <h1>Try out trading, Risk free.</h1>
+        <p class="main-text">Buy and sell stocks and cryptocurrencies.</p>
+        <p class="main-text">Compete with friends.</p>
+        <p class="main-text">All with live data.</p>
+      </div>
+      <RegisterDialog/>
+    </div>
+    <InfoSlider />
+    <InfoList />
+    <div class="buffer"></div>
   </main>
 </template>
 
 <script>
 // @ is an alias to /src
 import StocksListDrawer from '../components/stocks_list_drawer/StocksListDrawer.vue';
+import InfoSlider from '../components/info_slider/InfoSlider.vue'
+import RegisterDialog from '../components/register_dialog/RegisterDialog.vue'
+import InfoList from '../components/info-list/InfoList.vue'
 import Vuex from 'vuex';
 import { mapMutations, mapActions } from 'vuex';
 
 export default {
   name: 'Info',
   components: {
-    StocksListDrawer
+    InfoSlider,
+    RegisterDialog,
+    InfoList,
   },
   computed: {
     dark() {
@@ -30,3 +45,46 @@ export default {
   }
 };
 </script>
+
+<style lang="scss">
+#main-content{
+  background-image: url('../assets/info-background.jpg');
+  background-position: 43% 0%;
+  background-size: auto 100vh;
+  height: 78vh;
+  display:flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding-left: 10px;
+  button {
+    width: 40vw;
+    height: 100px;
+    margin: 20px;
+    font-family: $primary-font;
+    font-size: 1.5em;
+    background-color: rgb(75, 189, 84);
+  }
+}
+
+#CTA-register {
+  width: 40vw;
+  height: 100px;
+  margin: 20px;
+  font-family: $primary-font;
+  font-size: 1.5em;
+}
+.main-text{
+  font-size: 2.5em !important;
+  width: 85%;
+  padding-top: 20px;
+}
+.buffer{
+  height: 150px !important;
+}
+h1{
+  font-size: 4em;
+  font-family: $primary-font;
+  width: 85%;
+}
+
+</style>
