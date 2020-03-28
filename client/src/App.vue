fc65e1168633348cda08a225c6a9b4345eafcb7f
Bryce McMath <bryce.j.mcmath@gmail.com>
2020-03-27 23:40:38 -0700

    Move setRankingsData to App.vue created()
    Why:
    
    1) Needed access to that data before entering Leaderboard view

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <v-app :dark="dark" v-bind:class="[{ 'app--dark': dark }, { 'app--light': !dark }]">
+  <v-app
+    :dark="dark"
+    v-bind:class="[{ 'app--dark': dark }, { 'app--light': !dark }]"
+  >
     <Navbar></Navbar>
     <NavigationDrawer></NavigationDrawer>
     <v-content>
@@ -11,46 +14,47 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import Navbar from '@/components/navbar/Navbar.vue';
-import NavigationDrawer from '@/components/navigation_drawer/NavigationDrawer.vue';
-import FooterNavButton from '@/components/footer_nav_button/FooterNavButton.vue';
-import FooterNav from './components/footer_nav/FooterNav.vue';
-import Spinner from './components/spinner/Spinner.vue';
-import Dialog from './components/dialog/Dialog.vue';
-import { mapMutations, mapActions } from 'vuex';
+  import Vue from 'vue';
+  import Navbar from '@/components/navbar/Navbar.vue';
+  import NavigationDrawer from '@/components/navigation_drawer/NavigationDrawer.vue';
+  import FooterNavButton from '@/components/footer_nav_button/FooterNavButton.vue';
+  import FooterNav from './components/footer_nav/FooterNav.vue';
+  import Spinner from './components/spinner/Spinner.vue';
+  import Dialog from './components/dialog/Dialog.vue';
+  import { mapMutations, mapActions } from 'vuex';
 
-export default Vue.extend({
-  name: 'App',
-  components: {
-    Navbar,
-    NavigationDrawer,
-    FooterNav,
-    // Spinner
-  },
+  export default Vue.extend({
+    name: 'App',
+    components: {
+      Navbar,
+      NavigationDrawer,
+      FooterNav
+      // Spinner
+    },
 
-  computed: {
-    dark() {
-      return this.$store.state.ui.dark;
+    computed: {
+      dark() {
+        return this.$store.state.ui.dark;
+      },
+      loading() {
+        return this.$store.state.ui.ajaxInProgress;
+      },
+      tabSelected() {
+        const route = this.$route.path.toLowerCase();
+        if (route.includes('trade')) return 'trade';
+        if (route.includes('portfolios')) return 'portfolios';
+        if (route.includes('leaderboard')) return 'leaderboard';
+        return 'info';
+      }
     },
-    loading() {
-      return this.$store.state.ui.ajaxInProgress;
+    methods: {
+      ...mapActions(['checkUserAuth', 'setStocksData'])
     },
-    tabSelected() {
-      const route = this.$route.path.toLowerCase();
-      if (route.includes('trade')) return 'trade';
-      if (route.includes('portfolios')) return 'portfolios';
-      if (route.includes('leaderboard')) return 'leaderboard';
-      return 'info';
+    created: function() {
+      // check for a valid token whenever they visit
+      this.checkUserAuth();
+      this.setStocksData();
+      this.setRankingsData();
     }
-  },
-  methods: {
-    ...mapActions(['checkUserAuth', 'setStocksData'])
-  },
-  created: function() {
-    // check for a valid token whenever they visit
-    this.checkUserAuth();
-    this.setStocksData();
-  }
-});
+  });
 </script>
