162e024315e23d2bde756b2d884aa7c23bb55183
Bryce McMath <bryce.j.mcmath@gmail.com>
2020-03-27 17:21:31 -0700

    Refactor and restyle beginning of info view
    Why:
    
    1) Closer to mockup
    2) Mobile-first design

--- client/src/views/Info.vue
+++ client/src/views/Info.vue
@@ -2,10 +2,18 @@
   <main class="view-container">
     <div v-bind:class="['info-content', { 'info-content--dark': dark }]">
       <div id="intro-text">
-        <h1>Try out trading, Risk free.</h1>
-        <p class="main-text">Buy and sell stocks and cryptocurrencies.</p>
-        <p class="main-text">Compete with friends.</p>
-        <p class="main-text">All with live data.</p>
+        <h1>Lose all your money.</h1>
+        <h2>Without losing money.</h2>
+        <img
+          src="../assets/info-display.png"
+          alt="info-display"
+          class="info-display"
+        />
+        <aside class="main-text">
+          <p>Trade stocks and cryptocurrencies.</p>
+          <p>Compete with friends.</p>
+          <p>All with live data.</p>
+        </aside>
       </div>
       <RegisterDialog />
     </div>
@@ -37,10 +45,6 @@
       }
     },
     methods: {
-      showStocksDrawer: function() {
-        this.setStocksData();
-        this.$store.commit('toggleStocksDrawer');
-      },
       ...mapActions(['setStocksData'])
     }
   };
@@ -52,18 +56,21 @@
     background-position: 43% 0%;
     background-size: auto 100vh;
     height: 78vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
     padding-left: 10px;
 
     &--dark {
       background-image: url('../assets/info-background--dark.png');
     }
-    button {
+
+    img.info-display {
+      margin: 18px auto 0 auto;
+      width: 60%;
+    }
+
+    button.v-btn {
       width: 40vw;
       height: 100px;
-      margin: 20px;
+      margin: 0;
       font-family: $primary-font;
       font-size: 1.5em;
       background-color: rgb(75, 189, 84);
@@ -73,21 +80,31 @@
   #CTA-register {
     width: 40vw;
     height: 100px;
-    margin: 20px;
     font-family: $primary-font;
     font-size: 1.5em;
   }
+
   .main-text {
-    font-size: 2.5em !important;
-    width: 85%;
-    padding-top: 20px;
+    font-size: 22px !important;
+    width: 80%;
+
+    p {
+      margin: 0;
+    }
   }
+
   .buffer {
     height: 150px !important;
   }
+
   h1 {
-    font-size: 4em;
+    font-size: 36px;
     font-family: $primary-font;
-    width: 85%;
+    width: 95%;
+  }
+
+  h2 {
+    font-size: 24px;
+    width: 75%;
   }
 </style>
