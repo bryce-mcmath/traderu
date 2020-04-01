e56577b9dfc7f3ad8e51e71f4e12b1fea33103c0
Bryce McMath <bryce.j.mcmath@gmail.com>
2020-04-01 04:02:10 -0700

    Fix leaderboard bug
    What:
    
    1) While loading, cached data was being shown. I added a v-if to stop
    that

--- client/src/views/Leaderboard.vue
+++ client/src/views/Leaderboard.vue
@@ -11,73 +11,67 @@
     <div class="spinner-container" v-if="showSpinner">
       <Spinner></Spinner>
     </div>
-    <v-list :dark="dark">
-      <LeaderboardListItem
-        :portfolio="portfolio"
-        v-for="(portfolio, i) in rankData"
-        :key="`${i}`"
-      ></LeaderboardListItem>
+    <v-list v-if="!showSpinner" :dark="dark">
+      <LeaderboardListItem :portfolio="portfolio" v-for="(portfolio, i) in rankData" :key="`${i}`"></LeaderboardListItem>
     </v-list>
   </main>
 </template>
 
 <script>
-  import LeaderboardListItem from '../components/leaderboard_list_item/LeaderboardListItem.vue';
-  import Spinner from '../components/spinner/Spinner.vue';
-  import { mapActions } from 'vuex';
+import LeaderboardListItem from '../components/leaderboard_list_item/LeaderboardListItem.vue';
+import Spinner from '../components/spinner/Spinner.vue';
+import { mapActions } from 'vuex';
 
-  export default {
-    name: 'Leaderboard',
-    components: {
-      LeaderboardListItem,
-      Spinner
+export default {
+  name: 'Leaderboard',
+  components: {
+    LeaderboardListItem,
+    Spinner
+  },
+  methods: {
+    ...mapActions(['setRankingsData'])
+  },
+  created() {
+    this.setRankingsData();
+  },
+  computed: {
+    showSpinner() {
+      return this.$store.state.ui.ajaxInProgress;
     },
-    methods: {
-      ...mapActions(['setRankingsData'])
+    dark() {
+      return this.$store.state.ui.dark;
     },
-    created(){
-      this.setRankingsData();
-    },
-    computed: {
-      showSpinner() {
-        return this.$store.state.ui.ajaxInProgress;
-      },
-      dark() {
-        return this.$store.state.ui.dark;
-      },
-      rankData() {
-        // if (!Array.isArray(this.$store.state.apiData.allRankingsData)) return;
+    rankData() {
+      // if (!Array.isArray(this.$store.state.apiData.allRankingsData)) return;
 
-        //For /:id, only return portfolios around given ID
-        if (this.$route.params.id) {
-          //Add on array index as rank
-          const rankings = this.$store.state.apiData.allRankingsData.map(
-            (ranking, i) => ({ ...ranking, rank: i })
-          );
+      //For /:id, only return portfolios around given ID
+      if (this.$route.params.id) {
+        //Add on array index as rank
+        const rankings = this.$store.state.apiData.allRankingsData.map(
+          (ranking, i) => ({ ...ranking, rank: i })
+        );
 
-          //Get array index of portfolio with given ID
-          const portfolioIndex = rankings.indexOf(
-            rankings.find(
-              ranking => ranking.portfolioid == this.$route.params.id
-            )
-          );
+        //Get array index of portfolio with given ID
+        const portfolioIndex = rankings.indexOf(
+          rankings.find(ranking => ranking.portfolioid == this.$route.params.id)
+        );
 
-          if (portfolioIndex === 0) {
-            return rankings.slice(portfolioIndex, portfolioIndex + 2);
-          }
-          return rankings.slice(portfolioIndex - 1, portfolioIndex + 2);
+        if (portfolioIndex === 0) {
+          return rankings.slice(portfolioIndex, portfolioIndex + 2);
         }
-
-        //Return all portfolios otherwise
-        return this.$store.state.apiData.allRankingsData.map((ranking, i) => ({
-          ...ranking,
-          rank: i
-        }));
+        return rankings.slice(portfolioIndex - 1, portfolioIndex + 2);
       }
+
+      //Return all portfolios otherwise
+      return this.$store.state.apiData.allRankingsData.map((ranking, i) => ({
+        ...ranking,
+        rank: i
+      }));
     }
-  };
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-  @import 'leaderboard';
+@import 'leaderboard';
 </style>
